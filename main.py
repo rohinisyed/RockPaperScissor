b750eebadc2bdb5233cc29191473d383332f25f4
Rohini Syed <syedrohini1@gmail.com>
2020-11-07 20:03:45 +0000

    Main Game Logic done

--- main.py
+++ main.py
@@ -6,5 +6,25 @@ possible_choices = ["Rock", "Paper", "Scissor"]
 
 computer_choice = possible_choices[randint(0,2)]
 
-print(player_choice)
-print(computer_choice)
+print("Player Choice: ",player_choice)
+print("Computer Choice: ",computer_choice)
+
+if player_choice == computer_choice:
+  print ("Result: Tie")
+elif player_choice == "Rock": 
+  if computer_choice == "Scissor":
+    print ("Result: Win")
+  else:
+    print ("Result: Lose") 
+elif player_choice == "Scissor":
+  if computer_choice == "Paper":
+    print ("Result: Win")
+  else:
+    print ("Result: Lose")  
+elif player_choice == "Paper": 
+  if computer_choice == "Scissor":
+    print ("Result: Lose")
+  else: 
+    print ("Result: Win")  
+else:
+  print("Please try again, invalid choice!")
